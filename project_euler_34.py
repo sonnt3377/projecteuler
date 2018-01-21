e3d38857944f8d643c13385c152a4b2cc47a9869
Son Nguyen <4522835+sonthanhnguyen@users.noreply.github.com>
2018-01-21 21:04:20 +0100

    Project Euler 34

---
+++ project_euler_34.py
@@ -0,0 +1,61 @@
+"""
+    Project Euler 34: Digit factorials
+
+    145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+    Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+    Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+import time
+import math
+
+"""
+    Analysis: 
+    Assuming that the number contains n digits, then
+        A = The sum of the factorials of its digits is <= n*9! = 362880*n (= if all digits are 9)
+        B = The value of the number itself >= 10^(n-1) (= when the highest digit is 1, and all others are 0)
+    
+    For a certain value of n, B will always > A and as n increases beyond that value, A will never catch up,
+    so to find the solution, we only need to reach to the point where A may catch up with B.
+    
+    For n = 7, max(A) = 7*9! = 2,540,160, min(B) = 10^6 = 1,000,000 => there is a possibility for A = B
+    For n = 8, max(A) = 2,903,040, min(B) = 10^7 = 10,000,000 => no possibility for A = B.
+    For n > 8, same as n = 8
+    
+    So we only need to search for up to n = 7 digits, with the max possible value is 7*9!
+"""
+
+def do_work():
+    # A list containing pre-calculated factorial of number from 0 to 9
+    factorial_list = [math.factorial(i) for i in range(0, 10)]
+    # The sum of all numbers which are equal to the sum of the factorial of their digits.
+    sum = 0
+    # Max search range, as discussed in the Analysis part
+    MAX_RANGE = 2540160
+
+    # Start from 3, as 1 and 2 are excluded
+    for i in range(3, MAX_RANGE + 1):
+        number = i
+        factorial_sum = 0
+
+        # Finding sum of factorials of the digits
+        while number > 0:
+            digit = number % 10
+            number //= 10
+            factorial_sum += factorial_list[digit]
+
+        # If the number is equal to the sum of the factorials of its digits, it satisfies the requirement.
+        if factorial_sum == i:
+            print("The number is {0}".format(i))
+            sum += i
+
+    return sum
+
+def main():
+    start_time = time.time()
+    result = do_work()
+    print("Result is {0} found in {1} seconds".format(result, time.time() - start_time))
+
+if __name__ == "__main__":
+    main()
