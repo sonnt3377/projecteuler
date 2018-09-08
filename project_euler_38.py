c31ac1a743062dd310f925f68a30834b9b8a9f87
Son Nguyen <4522835+sonthanhnguyen@users.noreply.github.com>
2018-09-08 11:31:29 +0200

    Group a common method to own lib

--- project_euler_38.py
+++ project_euler_38.py
@@ -16,34 +16,14 @@
     with (1,2, ... , n) where n > 1?
 """
 import time
-
-
-def is_pandigital(*numbers):
-    """
-    Decide if a list of numbers, when concatenating to each other, create a pandigital
-
-    :param numbers: list of numbers to test
-    :return: True if the concatenating of numbers create a pandigital, False otherwise
-    """
-    test_string = ""
-    digit_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    for number in numbers:
-        test_string += str(number)
-
-    # The string is not pandigital if it contains 0 or if it has more than 9 digits
-    if len(test_string) != 9 or '0' in test_string:
-        return False
-
-    # The string is pandigital if each digit from 1 to 9 appears only once
-    return all(digit in test_string for digit in digit_list)
+import project_euler
 
 
 def find_max_pandigital():
     """
     Find the max 9-digit pandigital
     :return: the max value, plus the components generating that value
-    """
-    """
+
     Analysis:
     A 9-digit pandigital can be formed by:
         Number A x (1, 2)
@@ -59,39 +39,69 @@ def find_max_pandigital():
     max_value = 0
 
     # Search for all cases in A
-    for a in range(1000, 10000):
+    for four_digit_number in range(1000, 10000):
         # If a pandigital is found
-        if is_pandigital(a, a * 2):
-            pan_digital = int(str(a) + str(a * 2))
+        if project_euler.is_pandigital(four_digit_number, four_digit_number * 2):
+            pan_digital = int(str(four_digit_number) + str(four_digit_number * 2))
             if max_value < pan_digital:
                 max_value = pan_digital
 
     # Search for all cases in B
-    for b in range(100, 1000):
+    for three_digit_number in range(100, 1000):
         # If a pandigital is found
-        if is_pandigital(b, b * 2, b * 3):
-            pan_digital = int(str(b) + str(b * 2) + str(b * 3))
+        if project_euler.is_pandigital(three_digit_number, three_digit_number * 2, three_digit_number * 3):
+            pan_digital = int(str(three_digit_number) + str(three_digit_number * 2) + str(three_digit_number * 3))
             if max_value < pan_digital:
                 max_value = pan_digital
 
     # Search for all cases in C
-    for c in range(10, 100):
+    for two_digit_number in range(10, 100):
         # If a pandigital is found
-        if is_pandigital(c, c * 2, c * 3, c * 4):
-            pan_digital = int(str(c) + str(c * 2) + str(c * 3) + str(c * 4))
+        if project_euler.is_pandigital(two_digit_number, two_digit_number * 2,
+                                       two_digit_number * 3, two_digit_number * 4):
+            pan_digital = int(str(two_digit_number) + str(two_digit_number * 2)
+                              + str(two_digit_number * 3) + str(two_digit_number * 4))
             if max_value < pan_digital:
                 max_value = pan_digital
 
     # Search for all cases in D
-    for d in range(1, 10):
+    for one_digit_number in range(1, 10):
         # If a pandigital is found
-        if is_pandigital(d, d * 2, d * 3, d * 4, d * 5):
-            pan_digital = int(str(d) + str(d * 2) + str(d * 3) + str(d * 4) + str(d * 5))
+        if project_euler.is_pandigital(one_digit_number, one_digit_number * 2, one_digit_number * 3,
+                                       one_digit_number * 4, one_digit_number * 5):
+            pan_digital = int(str(one_digit_number) + str(one_digit_number * 2)
+                              + str(one_digit_number * 3) + str(one_digit_number * 4) + str(one_digit_number * 5))
             if max_value < pan_digital:
                 max_value = pan_digital
 
     return max_value
 
+# Analysis:
+#     The max 9-digit pandigital should start with digit 9, so the fixed number should
+#     start with 9 since it multiplies with 1 and then concated to be the first path
+#     of the 9-digit pandigital.
+#     A 9-digit pandigital can be formed by:
+#         Number A x (1, 2)
+#         Number B x (1, 2, 3)
+#         Number C x (1, 2, 3, 4)
+#         Number D x (1, 2, 3, 4, 5)
+#     Since the pandigital is 9-digit
+#         A should be a 4-digit number
+#         B should be a 3-digit number
+#         C should be a 2-digit number
+#         D should be a 1-digit number
+#     If the number is 2-digit (we only consider it in the form 9x, as we search for
+#     the max pandigital)
+#         When multiplying with (1, 2, 3), the longest number it creates has 8 digits
+#         When multiplying with (1, 2, 3, 4), the shortest number it creates has 11 digits
+#     If the number is 3-digit (we only consider it in the form 9x, as we search for
+#     the max pandigital)
+#         When multiplying with (1, 2), the longest number it creates has 7 digits
+#         When multiplying with (1, 2, 3), the shortest number it creates has 11 digits
+#     So the number can have only one digit i.e. 9 or 4 digits i.e 9xyz
+#     Regarding 4-digit numbers, the range should be from 9123 (smallest 4-digit with
+#     possibility to create a 9-digit pandigital) to 9876 (largest 4-digit with
+#     possibility to create a 9-digit pandigital).
 
 def find_max_pandigital_2():
     """
@@ -99,41 +109,12 @@ def find_max_pandigital_2():
 
     :return: the max value
     """
-    """
-    Analysis:
-
-    The max 9-digit pandigital should start with digit 9, so the fixed number should start with 9 since it multiplies
-    with 1 and then concated to be the first path of the 9-digit pandigital.
-
-    A 9-digit pandigital can be formed by:
-        Number A x (1, 2)
-        Number B x (1, 2, 3)
-        Number C x (1, 2, 3, 4)
-        Number D x (1, 2, 3, 4, 5)
-    Since the pandigital is 9-digit
-        A should be a 4-digit number
-        B should be a 3-digit number
-        C should be a 2-digit number
-        D should be a 1-digit number
-
-    If the number is 2-digit (we only consider it in the form 9x, as we search for the max pandigital)
-        When multiplying with (1, 2, 3), the longest number it creates has 8 digits
-        When multiplying with (1, 2, 3, 4), the shortest number it creates has 11 digits
-    If the number is 3-digit (we only consider it in the form 9x, as we search for the max pandigital)
-        When multiplying with (1, 2), the longest number it creates has 7 digits
-        When multiplying with (1, 2, 3), the shortest number it creates has 11 digits
-
-    So the number can have only one digit i.e. 9 or 4 digits i.e 9xyz
-
-    Regarding 4-digit numbers, the range should be from 9123 (smallest 4-digit with possibility to create a 9-digit
-    pandigital) to 9876 (largest 4-digit with possibility to create a 9-digit pandigital).
-    """
     # Max 9-digit pandigital, found when multiplying 9 with (1, 2, 3, 4, 5)
     max_value = 918273645
 
     # We search from large numbers down until finding a pandigital, and stop, since we already find the largest
     for i in range(9876, 9123, -1):
-        if is_pandigital(i, i * 2):
+        if project_euler.is_pandigital(i, i * 2):
             max_value = int(str(i) + str(i * 2))
             break
 
@@ -141,6 +122,9 @@ def find_max_pandigital_2():
 
 
 def main():
+    """
+    Test
+    """
     # Searching with 'brute force'
     start_time = time.time()
     result = find_max_pandigital()
