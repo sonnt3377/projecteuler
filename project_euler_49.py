aaaaaa495d772e26b1629679bfe6ea1f81726449
Son Nguyen <4522835+sonthanhnguyen@users.noreply.github.com>
2018-09-09 00:18:55 +0200

    Remove redundant code, use lib method

--- project_euler_49.py
+++ project_euler_49.py
@@ -1,42 +1,18 @@
 """
     Project Euler 49: Prime permutations
 
-    The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, is unusual in two ways:
-    (i) each of the three terms are prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+    The arithmetic sequence, 1487, 4817, 8147, in which each of the terms increases by
+    3330, is unusual in two ways:
+    (i) each of the three terms are prime, and,
+    (ii) each of the 4-digit numbers are permutations of one another.
 
-    There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, exhibiting this property, but there
-    is one other 4-digit increasing sequence.
+    There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+    exhibiting this property, but there is one other 4-digit increasing sequence.
 
     What 12-digit number do you form by concatenating the three terms in this sequence?
 """
 import time
-
-
-def generate_primes_using_sieving(lower_limit, upper_limit):
-    """
-    Generating a list of prime numbers between the two bounds.
-    We use the same approach as in other projects
-
-    :param lower_limit:
-    :param upper_limit:
-    :return: the prime list
-    """
-    # A boolean list, in which an item's index indicates a number corresponding to that index, and the item's value
-    # is True if that corresponding number is a prime, and False otherwise
-    primes = [True] * upper_limit
-    # 0 and 1 are not prime
-    primes[0], primes[1] = [False] * 2
-
-    for index, value in enumerate(primes):
-        # The first number with True is a prime, but the multiples of it are not
-        if value is True:
-            primes[index * 2::index] = [False] * (((upper_limit - 1) // index) - 1)
-
-    # The "primes" list above contains all numbers from 0 to upper_limit
-    # We only need primes from lower_limit to upper_limit
-    prime_list = [i for i in range(lower_limit, upper_limit) if primes[i] is True]
-
-    return prime_list
+import project_euler
 
 
 def is_permutation(number1, number2):
@@ -77,11 +53,13 @@ def is_permutation(number1, number2):
 
     # Compare the number of digits in number1 with those in number2
     for digit in dict1.keys():
-        # If number1 contains a digit that is not in number2, then the two numbers are not permutations
+        # If number1 contains a digit that is not in number2, then the two numbers are
+        # not permutations
         if digit not in dict2.keys():
             return False
+
         # If the two numbers contain different numbers of the same digit, they are not permutations.
-        elif dict1[digit] != dict2[digit]:
+        if dict1[digit] != dict2[digit]:
             return False
 
     return True
@@ -96,7 +74,7 @@ def do_work():
     :return: those three primes
     """
     # Generate 4-digit prime list, skipping the 1487 to exclude the existing solutions.
-    prime_list = generate_primes_using_sieving(1488, 10000)
+    prime_list = project_euler.sieve(10000, 1488)
 
     # Pick two primes out of the prime list
     for i in range(len(prime_list) - 1):
@@ -106,7 +84,8 @@ def do_work():
             # Check if the test number is also a prime
             # And if so, there three primes are permutation of each other
             if test_number in prime_list:
-                if is_permutation(prime_list[i], prime_list[j]) and is_permutation(prime_list[j], test_number):
+                if is_permutation(prime_list[i], prime_list[j]) \
+                   and is_permutation(prime_list[j], test_number):
                     return prime_list[i], prime_list[j], test_number
 
     # Test code
@@ -114,11 +93,15 @@ def do_work():
 
 
 def main():
+    """
+    Test
+    """
     start_time = time.time()
     primes = do_work()
     print("The three primes are: {0}, {1}, {2}".format(primes[0], primes[1], primes[2]))
-    print("The result is {0}, found in {1} seconds".format(str(primes[0]) + str(primes[1]) + str(primes[2]),
-                                                           time.time() - start_time))
+    print("The result is {0}, found in {1} seconds"
+          .format(str(primes[0]) + str(primes[1]) + str(primes[2]),
+                  time.time() - start_time))
 
 
 if __name__ == "__main__":
