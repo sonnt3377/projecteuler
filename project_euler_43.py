b2b636c97c3eed09d1ac406d85eca3040ce81341
Son Nguyen <4522835+sonthanhnguyen@users.noreply.github.com>
2018-09-08 12:13:56 +0200

    Use enumerate instead of interating with range and len methods

--- project_euler_43.py
+++ project_euler_43.py
@@ -1,8 +1,9 @@
 """
     Project Euler 43: Sub-string divisibility
 
-    The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some
-    order, but it also has a rather interesting sub-string divisibility property.
+    The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each
+    of the digits 0 to 9 in some order, but it also has a rather interesting sub-string
+    divisibility property.
 
     Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, we note the following:
     d2d3d4=406 is divisible by 2
@@ -18,7 +19,7 @@
 import itertools
 import time
 
-prime_list = [2, 3, 5, 7, 11, 13, 17]
+PRIME_LIST = [2, 3, 5, 7, 11, 13, 17]
 
 
 def are_sub_strings_divisible(string):
@@ -28,8 +29,8 @@ def are_sub_strings_divisible(string):
     :param string: the input string to test
     :return: True if all sub strings are divisible with such condition, False otherwise.
     """
-    for i in range(len(prime_list)):
-        if int(string[i + 1: i + 4]) % prime_list[i] != 0:
+    for index, value in enumerate(PRIME_LIST):
+        if int(string[index + 1: index + 4]) % value != 0:
             return False
 
     return True
@@ -39,14 +40,16 @@ def find_the_sum(string):
     """
     Return sum of all 0 to 9 pandigital numbers with divisible sub-strings
 
-    :param string: contains all digits from 0 to 9, each appears once, used as inputs to find 0 to 9 pandigital numbers
+    :param string: contains all digits from 0 to 9, each appears once, used as inputs
+     to find 0 to 9 pandigital numbers
     :return: the sum
     """
     the_sum = 0
 
     # Search for all 10-digit pandigital numbers
     for perm in itertools.permutations(string):
-        # Ignore '0' in the beginning as that string does not form 10-digit number, hence not 'that' pandigital
+        # Ignore '0' in the beginning as that string does not form 10-digit number,
+        # hence not 'that' pandigital
         if perm[0] == '0':
             continue
 
@@ -60,9 +63,13 @@ def find_the_sum(string):
 
 
 def main():
+    """
+    Test
+    """
     start_time = time.time()
     result = find_the_sum('0123456789')
-    print("The result is {0}, found in {1} seconds".format(result, time.time() - start_time))
+    print("The result is {0}, found in {1} seconds"
+          .format(result, time.time() - start_time))
 
 
 if __name__ == "__main__":
